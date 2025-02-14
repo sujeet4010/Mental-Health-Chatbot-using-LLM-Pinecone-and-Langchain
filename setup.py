7762eb62040cea0395a155d55e7c2d749d5ec5e6
Sujeet Kumar Gupta <sujeet4010@gmail.com>
2025-02-15 01:42:31 +0530

    creating folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Mental Health Chatbot',
+    version= '0.0.0',
+    author= 'Sujeet Kumar Gupta',
+    author_email= 'sujeet4010@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
